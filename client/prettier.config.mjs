86e0f98bec990fa42f5c4dfde3ccaf0c8f47090d
Mike McAvoy <218127259+mikemcavoydev@users.noreply.github.com>
2025-07-16 16:11:23 +0100

    style: run prettier across all default client files

--- client/prettier.config.mjs
+++ client/prettier.config.mjs
@@ -1,11 +1,11 @@
 const config = {
-  arrowParens: "always",
-  printWidth: 80,
-  singleQuote: false,
-  jsxSingleQuote: false,
-  semi: true,
-  trailingComma: "all",
-  tabWidth: 4,
+    arrowParens: "always",
+    printWidth: 80,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    semi: true,
+    trailingComma: "all",
+    tabWidth: 4,
 };
 
 export default config;
